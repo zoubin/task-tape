0716f2a2d927366b2cd135b2e1ba2c3470410293
zoubin <zoubin04@gmail.com>
2015-11-20 14:29:48 +0800

    Fix require babel

--- bin/task-tape.js
+++ bin/task-tape.js
@@ -7,16 +7,18 @@ program
   .option('-b --babel', 'tape babel code', false)
   .parse(process.argv)
 
-if (program.babel) {
-  require('babel-core/register')
-}
-
 var resolve = require('resolve')
 var hookRequire = require('../lib/hookRequire')
 var tape = require(
   resolve.sync('tape', { basedir: process.cwd() })
 )
 hookRequire(tape)
+
+if (program.babel) {
+  require(
+    resolve.sync('babel-core/register', { basedir: process.cwd() })
+  )
+}
 
 var path = require('path')
 var glob = require('glob')
